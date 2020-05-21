bb1a51aeeece54047af07d65c1ff9c8fbbbc46d8
dias1397 <joao_dias1397@hotmail.com>
2020-05-21 23:09:46 +0100

    Section 16 v1.0

---
+++ Section 16 - Lambda Expressions/src/com/joaodias/challenge4and5/Challenge4and5.java
@@ -0,0 +1,24 @@
+package com.joaodias.challenge4and5;
+
+import java.util.function.Function;
+
+public class Challenge4and5 {
+
+    public static void main(String[] args) {
+        Function<String, String> lambdaFunction = (String source) -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+        // Challenge 5
+        System.out.println(Challenge4and5.everySecondCharacter(lambdaFunction, "1234567890"));
+    }
+
+    private static String everySecondCharacter(Function<String, String> function, String functionArg) {
+        return function.apply(functionArg);
+    }
+}
---
+++ Section 16 - Lambda Expressions/src/com/joaodias/challenge6and7/Challenge6and7.java
@@ -0,0 +1,14 @@
+package com.joaodias.challenge6and7;
+
+import java.util.function.Supplier;
+
+public class Challenge6and7 {
+
+    public static void main(String[] args) {
+        Supplier<String> iLoveJava = () -> "I love Java!";
+
+        String supplierResult = iLoveJava.get();
+        System.out.println(supplierResult);
+    }
+
+}
