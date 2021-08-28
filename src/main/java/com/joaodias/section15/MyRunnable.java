eef416c20df2f611bfae63b24fc15bfc03593815
dias1397 <joao.dias1397@gmail.com>
2021-08-29 00:17:50 +0100

    Concurrency module setup

---
+++ src/main/java/com/joaodias/section15/MyRunnable.java
@@ -0,0 +1,10 @@
+package com.joaodias.section15;
+
+public class MyRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Hello from my runnables implementation of run.");
+    }
+
+}
